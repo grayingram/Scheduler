82f1bdd306dcf98318aaade3bf9ff1596bd2c0c9
gingram <grayson.ingram@att.net>
2018-08-20 09:47:08 -0500

    Added a method addemployee to the schedule class that bridges the connection between the scheduler and the crud operations

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -6,7 +6,19 @@ namespace Scheduler
     {
         static void Main(string[] args)
         {
-            Schedule weeklySchedule = new Schedule();
+            Schedule schedule = new Schedule();
+            Lawyer lawyer = new Lawyer();
+
+            if(lawyer.GetYesNo("Do you want to Add an employee?"))
+            {
+                schedule.AddEmployee();
+            }
+            else
+            {
+                Console.WriteLine("Okay then");
+            }
+            Console.WriteLine("Program end");
+            Console.ReadLine();
         }
     }
 }
--- Scheduler/Schedule.cs
+++ Scheduler/Schedule.cs
@@ -7,13 +7,24 @@ namespace Scheduler
     class Schedule
     {
         public List<Employee> Employees { get; set; } = new List<Employee>();
-        public int NumberOfEmployees { get; set; }
-        public int DaysWorkable { get; set; }
+        public int NumberOfEmployees { get; private set; }//debateable/deletable
+        public int DaysWorkable { get; private set; }
         public Lawyer Lawyer { get; set; }
+        private Creator Creator { get; set; } = new Creator();
         public Schedule()
         {            
             DaysWorkable = Lawyer.GetInt("How many days of the week is this company open for?");
             NumberOfEmployees = Lawyer.GetInt("How many employees work in this company?");
+        }
+        public void AddEmployee()
+        {
+            do
+            {
+                string firstname = Lawyer.GetResponse("What is the first name of this employee?");
+                string lastname = Lawyer.GetResponse("What is the last name of this employee?");
+                int vacations = Lawyer.GetInt("How many vacation days does this employee have for the year?");
+                Creator.AddEmployee(firstname, lastname, vacations);
+            } while (Lawyer.GetYesNo("Do you want to add another employee?"));
         }
         //for(int i = 0; i<NumberOfEmployees; i++)
         //    {
