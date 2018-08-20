e80182e65edeb3db756702acb9dc1cd7ed2a4dc0
gingram <grayson.ingram@att.net>
2018-08-20 10:14:11 -0500

    Added ability to not add employee if they exist

--- Scheduler/Schedule.cs
+++ Scheduler/Schedule.cs
@@ -9,8 +9,9 @@ namespace Scheduler
         public List<Employee> Employees { get; set; } = new List<Employee>();
         public int NumberOfEmployees { get; private set; }//debateable/deletable
         public int DaysWorkable { get; private set; }
-        public Lawyer Lawyer { get; set; }
+        public Lawyer Lawyer { get; set; } = new Lawyer();
         private Creator Creator { get; set; } = new Creator();
+        private Reader Reader { get; set; } = new Reader();
         public Schedule()
         {            
             DaysWorkable = Lawyer.GetInt("How many days of the week is this company open for?");
@@ -22,8 +23,14 @@ namespace Scheduler
             {
                 string firstname = Lawyer.GetResponse("What is the first name of this employee?");
                 string lastname = Lawyer.GetResponse("What is the last name of this employee?");
+                while(Reader.DoesEmployeeExist(firstname, lastname))
+                {
+                    Console.WriteLine("Sorry but that employee already exists, try again");
+                    firstname = Lawyer.GetResponse("What is the first name of this employee?");
+                    lastname = Lawyer.GetResponse("What is the last name of this employee?");
+                }
                 int vacations = Lawyer.GetInt("How many vacation days does this employee have for the year?");
-                if(Lawyer.GetYesNo("Are you sure you wnat to add " + firstname + " " + lastname + "to the employees"))
+                if(Lawyer.GetYesNo("Are you sure you wnat to add " + firstname + " " + lastname + " to the employees"))
                 {
                     Creator.AddEmployee(firstname, lastname, vacations);
                 }
